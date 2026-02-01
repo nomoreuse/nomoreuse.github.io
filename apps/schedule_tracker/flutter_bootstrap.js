880c14ad9f83108749bad2ba868f5fdc7d730897
dobby <devdobby@gmail.com>
2026-01-31 22:26:45 -0700

    app: schedule_tracker codex 6차 버전으로 수정

--- apps/schedule_tracker/flutter_bootstrap.js
+++ apps/schedule_tracker/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "636316327"
+    serviceWorkerVersion: "3471405361"
   }
 });
